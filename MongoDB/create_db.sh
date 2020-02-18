d7a8c0642771f81deb4560b0527bf8c98fe52d19
Stefano Cereda <stefanocereda@users.noreply.github.com>
2020-02-18 15:59:16 +0100

    Update create_db.sh

--- MongoDB/create_db.sh
+++ MongoDB/create_db.sh
@@ -1,9 +1,8 @@
 MONGODB_SERVER_IP=[server ip]
 RECORDCOUNT=3000000  # number of records
 WORKLOAD=a  # workload type
-LOAD_THREADS=16  # number of threads to use to create data 
+LOAD_THREADS=16  # number of threads used to create data 
+cd ycsb-0.17.0
 
 echo Creating DB with $RECORDCOUNT records...
-
-cd ycsb-0.17.0
 ./bin/ycsb load mongodb-async -s -P workloads/workload$WORKLOAD -threads $LOAD_THREADS  -p recordcount=$RECORDCOUNT -p mongodb.url=mongodb://$MONGODB_SERVER_IP:27017
