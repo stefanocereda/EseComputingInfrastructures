6712c901ce36cfde0831b62628ba44b0c10e6ef6
Stefano Cereda <stefanocereda@users.noreply.github.com>
2020-02-18 15:59:46 +0100

    Update run_ycsb.sh

--- MongoDB/run_ycsb.sh
+++ MongoDB/run_ycsb.sh
@@ -1,16 +1,12 @@
-MONGODB_SERVER_IP=localhost
-RECORDCOUNT=3000000
-WORKLOAD=a
-LOAD_THREADS=16
-RUN_THREADS=16
-DURATION=300
-
-
-echo Creating DB with $RECORDCOUNT records...
-./ycsb-0.15.0/bin/ycsb load mongodb-async -s -P workloads/workload$WORKLOAD -threads $LOAD_THREADS  -p recordcount=$RECORDCOUNT -p mongodb.url=mongodb://$MONGODB_SERVER_IP:27017
+MONGODB_SERVER_IP=[server ip]
+RECORDCOUNT=3000000  # number of records
+WORKLOAD=a  # workload type
+RUN_THREADS=16  # number of threads to use (= number of users N)
+DURATION=300  # duration of test in second
+cd ycsb-0.17.0
 
 echo Running test for X_max
-./ycsb-0.15.0/bin/ycsb run mongodb-async -s -P workloads/workload$WORKLOAD -threads $RUN_THREADS  -p recordcount=$RECORDCOUNT -p operationcount=0 -p maxexecutiontime=$DURATION -p mongodb.url=mongodb://$MONGODB_SERVER_IP:27017
+./bin/ycsb run mongodb-async -s -P workloads/workload$WORKLOAD -threads $RUN_THREADS  -p recordcount=$RECORDCOUNT -p operationcount=0 -p maxexecutiontime=$DURATION -p mongodb.url=mongodb://$MONGODB_SERVER_IP:27017
 
 for TGT_X in 100 200 300 500 1000 2000 3000; do
         echo Running test for X=$TGT_X
